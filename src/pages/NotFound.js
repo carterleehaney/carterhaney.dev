7a3aa39615c1e590ece4858c88bfb4485bfcd319
Carter Haney <carterleehaney@gmail.com>
2025-12-06 01:40:15 -0600

    Updated CI/CD, style 404 page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './NotFound.css';
+import NavBar from '../components/NavBar/NavBar';
+import Footer from '../components/Footer/Footer';
 
 function NotFound() {
   return (
     <div className="not-found">
-      <h1>404</h1>
-      <p>Page not found</p>
-      <Link to="/">Go back home</Link>
+      <NavBar />
+      <div className="not-found-content">
+        <h1>404</h1>
+        <p>Page not found</p>
+        <Link to="/">Go back home</Link>
+      </div>
+      <Footer />
     </div>
   );
 }
